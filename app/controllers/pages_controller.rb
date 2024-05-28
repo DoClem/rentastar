4c362a474c5e2e3e2246aadc3eb4cc37536e95dd
Pierre Scouarnec <p.scouarnec1@gmail.com>
2024-05-28 14:07:12 +0200

    git pull

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,6 @@
-# app/controllers/pages_controller.rb
 class PagesController < ApplicationController
   before_action :authenticate_user!, only: [:home]
 
   def home
-    # home action code
   end
 end
