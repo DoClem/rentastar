30b3f113449fe36cec79dace623ff6276634678f
thibault wanten <thibault.wanten@gmail.com>
2024-05-29 17:12:16 +0200

    Seed

--- app/models/celebrity.rb
+++ app/models/celebrity.rb
@@ -1,12 +1,44 @@
 class Celebrity < ApplicationRecord
 
-  belongs_to :user, dependent: :destroy
+  has_many :bookings, dependent: :destroy
 
-  has_many :booking
+require_relative '../config/environment'
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :address, presence: true
-  validates :price, presence: true, numericality: { greater_than: 0 }
+  API_KEY = 'tY8DbbwHy+hY3kfrM4i2fg==AH2KnOweoY3qRx6O'
+  NAMES = [
+    "Brad Pitt", "Scarlett Johansson", "Leonardo DiCaprio",
+    "Morgan Freeman", "Beyonce", "Jay-Z", "Emmanuel Macron", "Jean-Luc Mélenchon",
+    "Serena Williams", "LeBron James", "Teddy Riner", "Lionel Messi"
+  ]
+
+  def self.import_celebrities
+    puts "Cleaning up database..."
+    Celebrity.destroy_all
+    puts "Database cleaned"
+
+    created_count = 0
+
+    NAMES.each do |name|
+      break if created_count >= 10
+
+      puts "Importing celebrity data for #{name}"
+      url = "https://api.api-ninjas.com/v1/celebrity?name=#{CGI.escape(name)}"
+      response = URI.open(url, "X-Api-Key" => API_KEY).read
+      celebrity_data = JSON.parse(response).first
+
+      if celebrity_data
+        puts "Creating #{celebrity_data['name']}"
+        Celebrity.create(
+          name: celebrity_data["name"],
+          bio: celebrity_data["bio"],
+          profile_url: celebrity_data["image"]
+        )
+        created_count += 1
+      else
+        puts "No data found for #{name}"
+      end
+    end
+
+    puts "Celebrities created: #{created_count}"
+  end
 end
